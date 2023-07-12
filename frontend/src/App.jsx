846a22d229a4ff701395174c3805d8d580abfc3a
ANAIS CAVAILLE <anais.cavaille93@gmail.com>
2023-07-12 14:56:16 +0200

    installation de tailwind, creation des couleurs et themes et import des photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,16 @@
 import NavBar from "./components/NavBar";
 import AppRoutes from "./routes/AppRoutes";
 import Footer from "./components/Footer";
-import "./styles/App.module.scss";
+
+import "./index.css";
 
 function App() {
   return (
-    <div className="app-wrapper">
+    <>
       <NavBar />
-      <div className="page">        
-        <div className="content">
-          <AppRoutes />
-        </div>
-        <div className="footer">
-          <Footer />
-        </div>
-      </div>
-    </div>
+      <AppRoutes />
+      <Footer />
+    </>
   );
 }
 
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,105 +1,58 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../styles/NavBar.module.scss";
 
 function NavBar() {
   const [showLinks, setShowLinks] = useState(false);
   const handleShowLinks = () => setShowLinks(!showLinks);
-  const handleCloseNavbar = () => setShowLinks(false);
+  // const handleCloseNavbar = () => setShowLinks(false);
   return (
-    <div className={styles["block-nav"]}>
-      <div className={styles["logo-mobile"]}>
-        <Link to="/" onClick={handleCloseNavbar}>
-          <img
-            className={styles["img-logo-mobile"]}
-            src= "../src/assets/Images/logo-sleepy-dev-squad.png"
-            alt="Logo for mobile"
-          />
-        </Link>
-      </div>
-      <nav
-        className={`${styles.navbar} ${
-          showLinks ? styles["show-nav"] : styles["hide-nav"]
-        }`}
-      >
-        <ul className={styles["nav-links"]}>
-          <li className={styles["nav-li logo-desk"]}>
-            <Link
-              className={styles["nav-link"]}
-              to="/"
-              onClick={handleShowLinks}
-            >
-              <img
-                className={styles["img-logo-desk"]}
-                src="../src/assets/Images/logo-sleepy-dev-squad.png"
-                alt="Logo for desktop"
-              />
-            </Link>
-          </li>
-          <li className={styles["nav-li"]}>
-            <Link
-              className={styles["nav-link"]}
-              to="/informations"
-              onClick={handleShowLinks}
-            >
+    <nav className="bg-gray-500 font-mono">
+      <ul className="flex justify-between items-center">
+        <li>
+          <Link to="/" onClick={handleShowLinks}>
+            <img
+              className={styles["img-logo-desk"]}
+              src="../src/assets/Images/logo-sleepy-dev-squad.png"
+              alt="Logo for desktop"
+            />
+          </Link>
+        </li>
+        <div className="flex justify-between w-4/12 mr-12">
+          <li>
+            <Link to="/informations" onClick={handleShowLinks}>
               <h1>Informations</h1>
             </Link>
           </li>
-          <li className={styles["nav-li"]}>
-            <Link
-              className={styles["nav-link"]}
-              to="/Page3"
-              onClick={handleShowLinks}
-            >
+          <li>
+            <Link to="/Page3" onClick={handleShowLinks}>
               <h1>Page 3</h1>
             </Link>
           </li>
-          <li className={styles["nav-li"]}>
-            <Link
-              className={styles["nav-link"]}
-              to="/Page4"
-              onClick={handleShowLinks}
-            >
+          <li>
+            <Link to="/Page4" onClick={handleShowLinks}>
               <h1>Page 4</h1>
             </Link>
           </li>
-          <li className={styles["nav-li"]}>
-            <Link
-              className={styles["nav-link"]}
-              to="/Page5"
-              onClick={handleShowLinks}
-            >
+          <li>
+            <Link to="/Page5" onClick={handleShowLinks}>
               <h1>Page 5</h1>
             </Link>
           </li>
-          <li className={styles["nav-li"]}>
-            <Link
-              className={styles["nav-link"]}
-              to="/Page6"
-              onClick={handleShowLinks}
-            >
+          <li>
+            <Link to="/Page6" onClick={handleShowLinks}>
               <h1>Page 6</h1>
             </Link>
           </li>
-          <li className={styles["nav-li"]}>
-            <Link
-              className={styles["nav-link"]}
-              to="/Page7"
-              onClick={handleShowLinks}
-            >
-                <h1>Page 7</h1>
+          <li>
+            <Link to="/Page7" onClick={handleShowLinks}>
+              <h1>Page 7</h1>
             </Link>
           </li>
-        </ul>
-        <button
-          className={styles["nav-burger"]}
-          type="button"
-          onClick={handleShowLinks}
-        >
-          <span className={styles.burgerBar} />
-        </button>
-      </nav>
-    </div>
+        </div>
+      </ul>
+      <button type="button" onClick={handleShowLinks}></button>
+    </nav>
   );
 }
 export default NavBar;
