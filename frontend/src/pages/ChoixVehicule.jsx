2c60abf929428eb0aa92735ef3356144fc4aca07
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-07-12 17:45:15 +0200

    filtres en cours

--- frontend/src/pages/ChoixVehicule.jsx
+++ frontend/src/pages/ChoixVehicule.jsx
@@ -6,7 +6,17 @@ function ChoixVehicule() {
         src="../src/assets/Images/fresque.jpg"
         alt="foule derrière un carrosse avec chevaux"
       />
-      <div class="flex"></div>
+      <div>
+        <select
+            // onChange={(e) => setStageFilter(e.target.value)}
+        >
+          <option value="All">Toutes les scènes</option>
+          <option value="Flying High">Flying High</option>
+          <option value="Sonic Sphere">Sonic Sphere</option>
+          <option value="Electronic Dawn">Electronic Dawn</option>
+          <option value="Sunset Stage">Sunset Stage</option>
+        </select>
+      </div>
     </>
   );
 }
