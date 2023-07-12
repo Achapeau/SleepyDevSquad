edd76834bce5d5b5a4d1a84ceecff5cfb1ea4a5b
ANNIE CROTEAU <anniecroteauc@gmail.com>
2023-07-12 21:42:07 +0200

    page choixvehicule fonctionnelle

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,21 +1,21 @@
 function Footer() {
   return (
     <div>
-      <div className="bg-blue w-full h-20 absolute bottom-0 flex justify-center items-center">
-        <p className="font-mono text-almostWhite"> 2023 © Created by Sleepy Dev Squad </p>
+      <div className="bg-blue w-full h-20  flex justify-center items-center">
+        <p className="font-mono text-almostWhite">
+          {" "}
+          2023 © Created by Sleepy Dev Squad{" "}
+        </p>
         <div>
-            <img
-              src="../src/assets/Images/logo-sleepy-dev-squad.png"
-              alt="Logo for desktop"
-              className="h-20 p-5"
-            />
-          </div>
+          <img
+            src="../src/assets/Images/logo-sleepy-dev-squad.png"
+            alt="Logo for desktop"
+            className="h-20 p-5"
+          />
+        </div>
       </div>
     </div>
-  
-
-  
-  )
+  );
 }
 
 export default Footer;
--- frontend/src/pages/ChoixVehicule.jsx
+++ frontend/src/pages/ChoixVehicule.jsx
@@ -1,4 +1,28 @@
+import Proptypes from "prop-types";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import Vehicule from "../components/Vehicule";
+
 function ChoixVehicule() {
+  //Récupérer les informations depuis l'API
+  const [vehiclesData, setVehiclesData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/vehicules`)
+      .then((response) => {
+        setVehiclesData(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  if (vehiclesData.length > 0) {
+    console.log(vehiclesData);
+  }
+
+  const km = 50;
+
   return (
     <>
       <img
@@ -6,16 +30,46 @@ function ChoixVehicule() {
         src="../src/assets/Images/fresque.jpg"
         alt="foule derrière un carrosse avec chevaux"
       />
-      <div>
-        <select
-            // onChange={(e) => setStageFilter(e.target.value)}
-        >
-          <option value="All">Toutes les scènes</option>
-          <option value="Flying High">Flying High</option>
-          <option value="Sonic Sphere">Sonic Sphere</option>
-          <option value="Electronic Dawn">Electronic Dawn</option>
-          <option value="Sunset Stage">Sunset Stage</option>
-        </select>
+      <div class="flex flex-col md:flex-row">
+        <div class="w-1/6">
+          {/* <div class="flex">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn m-1">
+            Click
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div> */}
+          <select
+          // onChange={(e) => setStageFilter(e.target.value)}
+          >
+            <option value="tri">Trier les véhicules</option>
+            <option value="prixCroissant">Par prix décroissant</option>
+            <option value="prixDécroissant">Par prix croissant</option>
+            <option value="duréeCroissante">Par durée croissante</option>
+            <option value="durée Décroissante">Par durée décroissante</option>
+          </select>
+        </div>
+        <div class="flex flex-col md:flex-row md:w-5/6 md:p-8 overflow-hidden flex-wrap justify-center items-center">
+          {vehiclesData.length > 0 &&
+            vehiclesData.map((vehicle) => (
+              <Vehicule
+                image={vehicle.image}
+                name={vehicle.name}
+                prix={vehicle.prix * km}
+                temps={km / vehicle.vitesse}
+              />
+            ))}
+        </div>
       </div>
     </>
   );
