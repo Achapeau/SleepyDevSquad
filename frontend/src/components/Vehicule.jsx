085287a82d2906f14213c96a716ffc43e78d042c
Laurene Buttelli <laurene.buttelli@gmail.com>
2023-07-13 13:18:21 +0200

    add css

--- frontend/src/components/Vehicule.jsx
+++ frontend/src/components/Vehicule.jsx
@@ -10,13 +10,15 @@ function Vehicule({ image, name, prix, temps, id, description }) {
   };
 
   return (
-    <div className="w-4/5 h-1/2 flex flex-col justify-between items-center text-blue font-mono my-10 md:w-1/3 md:px-4 lg:px-10  md:h-1/6 drop-shadow-xl">
-      <img src={image} value={id} className="h-2/5 hover:animate-waving-caross active:animate-waving-caross transition-transform object-contain" onClick={handleClick} />
-      <h1 className="text-2xl">{name}</h1>
-      <p className="text-base lg:text-xl">Prix : {prix} Ecus</p>
-      <p className="text-base lg:text-xl">
+    <div className="flex flex-col justify-between items-center text-blue font-mono my-10 drop-shadow-xl">
+      <img src={image} value={id} className="h-44 hover:animate-waving-caross active:animate-waving-caross transition-transform object-contain" onClick={handleClick} />
+      <div className="flex flex-col items-center justify-center mx-10 text-center">
+      <h1 className="text-2xl my-5">{name}</h1>
+      <p className="text-xl my-2">Prix : {prix} Ecus</p>
+      <p className="text-xl">
         Temps jusqu'à destination : {Math.round(temps)} heures
       </p>
+      </div>
       <button
         className="bg-red text-almostWhite hover:bg-blue hover:text-yellow p-5 flex justify-center items-center font-sans drop-shadow-lg rounded-2xl hover:scale-110 m-5 w-4/5 lg:w-3/4 h-1/6 text-4xl md:text-lg lg:text-3xl transform active:scale-75 active:bg-blue active:text-almostwhite transition-transform"
         type="button"
